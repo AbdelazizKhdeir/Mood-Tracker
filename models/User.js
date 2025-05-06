9d72033a6a52923fcd83f34ee7f4385e91b8b905
AbdelazizKhdeir <abdalazeezkhdeir@gmail.com>
2025-05-06 20:00:42 +0300

    New

--- models/User.js
+++ models/User.js
@@ -2,14 +2,15 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: {
- type: String,
- required: true, 
-unique: true
- },
+    type: String,
+    required: true,
+    unique: true,
+  },
   password: {
- type: String,
- required: true
- },
+    type: String,
+    required: true,
+  },
+  timestamp: true,
 });
 
 module.exports = mongoose.model("User", userSchema);
